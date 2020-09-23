adadc539660889185e68bf7b5d17e249eb60276f
DanielRY <danielrojasy@upeu.edu.pe>
2020-09-23 09:05:50 -0500

    v2

--- src/main/java/pe/edu/upeu/sisventas_wannacry/dao/RolDao.java
+++ src/main/java/pe/edu/upeu/sisventas_wannacry/dao/RolDao.java
@@ -1,5 +1,11 @@
 package pe.edu.upeu.sisventas_wannacry.dao;
-
-public interface RolDao {
-
+import java.util.List;
+import java.util.Map;
+import pe.edu.upeu.sisventas_wannacry.entity.Rol;
+public interface RolDao{
+	int create(Rol r);
+	int update(Rol r);
+	int delete(int id);
+	Rol read(int id);
+	List<Map<String,Object>>readAll();
 }
--- src/test/java/pe/edu/upeu/sisventas_wannacry/SisventasWannacryApplicationTests.java
+++ src/test/java/pe/edu/upeu/sisventas_wannacry/SisventasWannacryApplicationTests.java
@@ -1,13 +1,20 @@
 package pe.edu.upeu.sisventas_wannacry;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import pe.edu.upeu.sisventas_wannacry.dao.RolDao;
+import pe.edu.upeu.sisventas_wannacry.entity.Rol;
 
 @SpringBootTest
 class SisventasWannacryApplicationTests {
-
+	@Autowired
+	private RolDao rolDao;
 	@Test
 	void contextLoads() {
+		Rol r=new Rol();
+		System.out.println(rolDao.delete(2));
+		/*System.out.println(rolDao.read(1).getNomrol());*/
 	}
-
 }
